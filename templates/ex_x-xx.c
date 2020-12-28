a062ff08dbeff5832630d61ba8039fb3fafeb7b2
Lee Howard <leegibsonhoward@gmail.com>
2020-12-27 22:43:16 -0800

    Add templates
    - Reusable files with comments for examples and exercises

---
+++ templates/ex_x-xx.c
@@ -0,0 +1,16 @@
+/********************************************************
+*
+* Chapter: XX - Chapter
+*
+* ex_x-xx.c -- Brief description
+*
+* Author: Person who created the program
+*
+* Date: XX/XX/XXXX
+*
+* Purpose: Explain the purpose of the program.
+*
+* Usage:
+*       Explain how to use the program.
+*
+********************************************************/
---
+++ templates/x-xx_name.c
@@ -0,0 +1,18 @@
+/********************************************************
+*
+* Chapter: XX - Chapter
+* 
+* Listing: X-XX - Listing Number
+*
+* x-xx_name.c -- Brief description
+*
+* Author: Stephen Prata
+*
+* Date: 2005
+*
+* Purpose: Explain the purpose of the program.
+*
+* Usage:
+*       Explain how to use the program.
+*
+********************************************************/
