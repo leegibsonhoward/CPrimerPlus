fd9efb7508c94c5e28243888e7cb168ce5d64ca4
Lee Howard <leegibsonhoward@gmail.com>
2021-01-03 14:14:21 -0800

    Add 1-02_concrete.c example

---
+++ chapter_01/examples/1-02_concrete.c
@@ -0,0 +1,26 @@
+/********************************************************
+*
+* Chapter: 01
+*
+* 1-02_concrete.c -- Programming mechanics
+* 
+* Listing: 1-02
+*
+* Author: Stephen Prata
+*
+* Date: 2005
+*
+* Purpose: Get a feel for what c program consists of
+*
+* Usage:
+*        Prints a fact about concrete.  
+*
+********************************************************/
+#include <stdio.h>
+
+int main()
+{
+    printf("Concrete contains gravel and cement.\n");
+    
+    return 0;
+}
