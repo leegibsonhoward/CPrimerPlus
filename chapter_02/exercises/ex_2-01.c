aacefb1656fdcecba86fe96ca9ce68124746d135
Lee Howard <leegibsonhoward@gmail.com>
2021-02-18 20:25:12 -0800

    Add ex_2-01.c exercise
    - Use multiple printf to output my name

---
+++ chapter_02/exercises/ex_2-01.c
@@ -0,0 +1,34 @@
+/********************************************************
+*
+* Chapter: 2
+*
+* ex_02-01.c -- Print first and last name using printf
+*
+* Author: Lee Howard
+*
+* Date: 02/18/2021
+*
+* Purpose: Learn to use printf to print statements
+*
+* Usage:
+*       Just run the program no input required
+*
+********************************************************/
+
+#include <stdio.h>
+
+int main(void)
+{
+    /* first print statement */
+    printf("Lee Howard\n");
+
+    /* second print statement */
+    printf("Lee\n");
+    printf("Howard\n");
+
+    /* third print statement */
+    printf("Lee ");
+    printf("Howard\n");
+
+    return 0;
+}
