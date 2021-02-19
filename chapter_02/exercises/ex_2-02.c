41907a5a9c5bcd24bef10d9378ce99c3c7268301
Lee Howard <leegibsonhoward@gmail.com>
2021-02-18 20:32:24 -0800

    Add ex_2-02.c exercise
    - Use printf to output my name and address

---
+++ chapter_02/exercises/ex_2-02.c
@@ -0,0 +1,27 @@
+/********************************************************
+*
+* Chapter: 2
+*
+* ex_2-02.c --  Print name and address using printf
+*
+* Author: Lee Howard
+*
+* Date: 02/18/2021
+*
+* Purpose: More practice using printf to output
+*
+* Usage:
+*       Just run the program no input required
+*
+********************************************************/
+
+#include <stdio.h>
+
+int main(void)
+{
+    printf("Lee Howard\n");
+    printf("123 Somewhere\n");
+    printf("Palo Alto, Ca 94303\n");
+
+    return 0;
+}
