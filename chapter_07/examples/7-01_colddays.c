7ce22909f47093f5747bb91cba99ba9978d70308
Lee Howard <leegibsonhoward@gmail.com>
2021-03-16 17:54:30 -0700

    Add 7-01_colddays.c example

---
+++ chapter_07/examples/7-01_colddays.c
@@ -0,0 +1,44 @@
+/********************************************************
+*
+* Chapter: 07
+*
+* 7-01_colddays.c -- Percentage of days below freezing
+* 
+* Listing: 7-01
+*
+* Author: Stephen Prata
+*
+* Date: 2005
+*
+* Purpose: Revisit if statements
+*
+* Usage:
+*      Enter a list of low temperatures and outputs 
+*      percentage.
+*
+********************************************************/
+#include <stdio.h>
+
+int main(void)
+{
+    const int FREEZING = 0;
+    float temperature;
+    int cold_days = 0;
+    int all_days = 0;
+
+    printf("Enter the list of daily low temperatures.\n");
+    printf("Use Celsius, and enter q to quit.\n");
+    while (scanf("%f", &temperature) == 1)
+    {
+        all_days++;
+        if (temperature < FREEZING)
+            cold_days++;
+    }
+    if(all_days != 0)
+        printf("%d days total: %.1f%% were below freezing.\n",
+            all_days, 100.0 * (float)cold_days / all_days);
+    if (all_days == 0)
+        printf("No data entered!\n");
+
+    return 0;
+}
