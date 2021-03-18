38afe56f9cdf05d2424dcd4200ab7512d41b378c
Lee Howard <leegibsonhoward@gmail.com>
2021-03-17 18:23:03 -0700

    Add ex_7-10.c exercise
    - Program is incomplete

---
+++ chapter_07/exercises/ex_7-10_incomplete.c
@@ -0,0 +1,34 @@
+/********************************************************
+*
+* Chapter: 7
+*
+* ex_07-09.c -- Pay rates with menu
+*
+* Author: Lee Howard
+*
+* Date: 03/17/2021
+*
+* Purpose: More practice with conditional statements
+*
+* Usage: 
+*       Enter number of hours worked for a week 
+*       and outputs 
+*       
+*
+********************************************************/
+
+/*
+   Write a program that lets the user specify the tax
+   category and the taxable income and that then calculates
+   the tax. Use a loop so that the user can enter several
+   tax cases.
+
+*/
+
+#include <stdio.h>
+
+int main(void)
+{
+   
+   return 0;
+}
